46a051d0556bf8723a5021e5467b1cceb0b78fc3
Tom Carfano <tomcarfano@TC-Macbook-Pro.lan>
2023-12-03 23:24:11 -0800

    add app.get for home page

--- server/index.js
+++ server/index.js
@@ -11,6 +11,9 @@ app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'))
+});
 
 app.listen(PORT, (error) => {
     if(!error){
