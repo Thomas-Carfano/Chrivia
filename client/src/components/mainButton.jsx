89a76fc26087083c162e62b1b558d24d37edd358
Tom Carfano <tomcarfano@TC-Macbook-Pro.lan>
2023-12-05 22:04:57 -0800

    create login signup and game page skeleton with some code

---
+++ client/src/components/mainButton.jsx
@@ -0,0 +1,18 @@
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+
+
+const CreateGame = () => {
+  return (
+    <>
+    <Stack spacing={2} direction="row">
+      <Button>Start A New Game</Button>
+      <Button href='/game'>Game</Button>
+      <Button href='/login'>Login</Button>
+      <Button href='/signup'>SignUp</Button>
+    </Stack>
+    </>
+  );
+}
+
+export default CreateGame;
