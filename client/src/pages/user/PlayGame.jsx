c487a7927c6c25a6849379c656bc865e979ed09c
Tom Carfano <tomcarfano@TC-Macbook-Pro.lan>
2023-12-19 23:53:30 -0800

    add web sockets to website

--- client/src/pages/user/PlayGame.jsx
+++ client/src/pages/user/PlayGame.jsx
@@ -1,4 +1,4 @@
-import '../App.css';
+import '../../App.css';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 
--- server/index.js
+++ server/index.js
@@ -4,10 +4,15 @@ const path = require("path");
 const bodyParser = require("body-parser");
 require("dotenv").config();
 const morgan = require("morgan");
+const cors = require("cors");
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
-const PORT = 3030;
 app.use(morgan("dev"));
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
@@ -35,12 +40,36 @@ app.get('/play', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/dist/index.html'))
 });
 
-
-
-app.listen(PORT, (error) => {
+server.listen(process.env.PORT, (error) => {
     if(!error){
-        console.log(`Server is listening on ${PORT}`)
+        console.log(`Server is listening on ${process.env.PORT}`)
     } else {
         console.log('not working')
     }
 });
+
+//Start of Game Code
+
+io.on("connection", (socket) => {
+    const joinRoom = (roomName, roomNum) => {
+      if (!io.sockets.adapter.rooms.get(roomName)) {
+        console.log("No Users in Room");
+        socket.join(roomName);
+      }
+        else if (io.sockets.adapter.rooms.get(roomName).size < 3) {
+            socket.join(roomName);
+            rooms.roomName.addPlayer();
+        }
+            else {
+                roomNum++;
+                roomName = `Room${roomNum}`;
+                joinRoom(roomName, roomNum);
+            }
+    }
+    socket.on("join", (join) => {
+        let roomNum = 1;
+        let roomName = `Room${roomNum}`;
+        joinRoom(roomName, roomNum);
+      });
+});
+
